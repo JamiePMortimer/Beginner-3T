f7432edba0cdf96316779d71fd297c90b437df21
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-01-26 20:10:43 +0000

    Completed New Game Modal

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const newGameBtn = document.getElementById('new-game-btn');
-const newGameModal = document.getElementById('new-game-modal');
+const newGameModal = document.querySelector('.new-game-modal');
 const backdrop = document.getElementById('backdrop-modal');
 const p1colour = document.getElementById('p1col');
 const p2colour = document.getElementById('p2col');
-const p1name = document.querySelector('.p1nameSelect')
-const p2name = document.querySelector('.p2nameSelect')
+const p1name = document.querySelector('.p1nameSelect');
+const p2name = document.querySelector('.p2nameSelect');
+const start = document.querySelector('.start-btn');
+const player1Name = document.querySelector('.play1');
+const player2Name = document.querySelector('.play2');
+const player1NameInput = document.querySelector('.p1Name');
+const player2NameInput = document.querySelector('.p2Name');
+
 let winner;
 let p1;
 let p2;
 
-p1colour.addEventListener('focusout', ()=>{
-  p1name.style.color = p1colour.value
-})
-p2colour.addEventListener('focusout', ()=>{
-  p2name.style.color = p2colour.value
-})
-newGameBtn.addEventListener('click', () =>{
-newGameModal.classList.remove('hide');
+p1colour.addEventListener('focusout', () => {
+  p1name.style.color = p1colour.value;
+});
+p2colour.addEventListener('focusout', () => {
+  p2name.style.color = p2colour.value;
+});
+newGameBtn.addEventListener('click', () => {
+  newGameModal.classList.remove('hide');
+});
 
-})
+start.addEventListener('click', startGame);
+
+function startGame() {
+  if (player1NameInput.value === '' || player2NameInput.value === '') {
+    return;
+  } else {
+    newGameModal.classList.add('hide');
+    player1Name.textContent = player1NameInput.value;
+    player2Name.textContent = player2NameInput.value;
+    player1NameInput.value = null;
+    player2NameInput.value = null;
+    player1Name.style.color = p1colour.value;
+    player2Name.style.color = p2colour.value;
+  }
+}
 
 // if (
 //   (a === 'X' && b === 'X' && c === 'X') ||
